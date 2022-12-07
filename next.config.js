f61558bd9fa30768cd9f2e82bbc0c91953966558
tasola <petter.tk@hotmail.com>
2022-12-07 13:35:10 +0100

    Add keyword chips, basic styling and limited keywords result

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  styledComponents: true,
 }
 
 module.exports = withBundleAnalyzer(nextConfig)
