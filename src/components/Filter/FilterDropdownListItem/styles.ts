c52f208703fedbc4a678d792f14e8e4ff668e58d
tasola <petter.tk@hotmail.com>
2022-12-08 14:35:53 +0100

    Add logic for open drop down - Switch to emotion

--- src/components/Filter/FilterDropdownListItem/styles.ts
+++ src/components/Filter/FilterDropdownListItem/styles.ts
@@ -1,4 +1,4 @@
-import styled from "styled-components";
+import styled from "@emotion/styled";
 import { ButtonWithoutDefaultStyle } from "../../../styles/global";
 
 export const ListItem = styled.li`
