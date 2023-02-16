6540b10a3f62a074bd5118156ebc78ab288073b2
Håkan Eriksson <hakan.eriksson@frankfam.co>
2023-02-16 13:36:13 +0100

    Replace placeholder pictures with new ones

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import PosterList from '../components/PosterList/PosterList'
 import Hero from '../components/Hero/Hero'
-import hero from '/public/images/hero.png'
-import handsTogether from '/public/images/hands-together.png'
+import campus from '/public/images/campus-students.jpeg'
+import students from '/public/images/lesson-students.jpeg'
 import DocumentIcon from '/public/icons/document.svg'
 import ReuseIcon from '/public/icons/reuse.svg'
 import GroupIcon from '/public/icons/group.svg'
@@ -32,8 +32,8 @@ export default function Home() {
 
 const heroProps = {
   image: {
-    alt: 'Students sitting in chairs gathered around a table with notepads, pens, and a laptop in the middle',
-    src: hero,
+    alt: 'Five students sitting outside taking notes',
+    src: campus,
   },
   title: 'Let your teaching material come alive',
   body: 'Our mission is to enable teachers and trainers to use, co-create and share open-licensed teaching and learning material anywhere in the world for delivery online and in the classroom',
@@ -69,8 +69,8 @@ const posterProps = {
 
 const textImageProps = {
   image: {
-    alt: 'Overhead view of hands from various people coming in from a circle towards the center',
-    src: handsTogether,
+    alt: 'Four students gathered around a laptop',
+    src: students,
   },
   title: 'Want to develop learning material or provide feedback?',
   body: 'Global collaboration on course material means that it is always up to date – especially important in cutting edge fields of research such as energy.\n\nTo enquire about access to the teaching kit website to develop and share your own material, please e-mail [teaching@climatecompatiblegrowth.com](mailto:teaching@climatecompatiblegrowth.com)\n\nTo provide feedback on the teaching kit website, please e-mail [platform@climatecompatiblegrowth.com](mailto:platform@climatecompatiblegrowth.com)',
