5db7ea133677897d56e7e540d9ce53235983f14e
RickardssonJ <rickardssonjimmy@gmail.com>
2023-02-20 17:32:48 +0100

    add zendo feature

---
+++ src/pages/api/zenodo.ts
@@ -0,0 +1,45 @@
+import axios from 'axios'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { LearningMaterialType } from '../../types'
+
+type StrapiWebhookRequest = NextApiRequest & {
+  body: {
+    model?: LearningMaterialType
+    entry?: { id?: number }
+  }
+}
+
+const ACCESS_TOKEN =
+  'paW9vfAwam6XKNjAJxa3VLbwSVYxNTJ00mYtOzQ4wNKDVj4cEOzEqfzbzM4d'
+
+const requestParams = {
+  params: {
+    access_token: ACCESS_TOKEN,
+  },
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}
+
+const config = {
+  headers: { Authorization: `Bearer ${ACCESS_TOKEN}` },
+}
+
+export default async function handler(
+  req: StrapiWebhookRequest,
+  res: NextApiResponse
+) {
+  console.log(req)
+  console.log(res)
+
+  axios
+    .post('https://zenodo.org/api/deposit/depositions', requestParams, config)
+    .then((response) => {
+      console.log(response.status)
+      // 201
+      console.log(response.data)
+    })
+    .catch((error) => {
+      console.log(error.response.data)
+    })
+}
