f09fdede98fbce4ecd4a0335a1f3c6a6bcd28858
tasola <petter.tk@hotmail.com>
2023-03-06 15:42:24 +0100

    Fix authors filter dropdown after having changed to firstname+lastname

--- src/shared/requests/authors/authors.ts
+++ src/shared/requests/authors/authors.ts
@@ -1,10 +1,19 @@
-import axios from "axios";
-import { Author, Data } from "../../../types";
-import { ResponseArray } from "../types"
+import axios from 'axios'
+import { Author, Data } from '../../../types'
+import { ResponseArray } from '../types'
 
 const ENDPOINT = `${process.env.NEXT_PUBLIC_STRAPI_API_URL}/authors`
 
-export const searchForAuthors = async (searchTerm: string): Promise<Data<Author>[]> => {
-  const response: ResponseArray<Author> = await axios.get(`${ENDPOINT}?filters[Name][$containsi]=${searchTerm}`)
-  return response.data.data;
+export const searchForAuthors = async (
+  searchTerm: string
+): Promise<Data<Author>[]> => {
+  console.log(searchTerm)
+  const filters =
+    searchTerm !== ''
+      ? `?filters[$or][0][FirstName][$containsi]=${searchTerm}&populate=*&filters[$or][1][LastName][$containsi]=${searchTerm}&filters[$or][2][Email][$containsi]=${searchTerm}&filters[$or][3][ORCID][$containsi]=${searchTerm}`
+      : ''
+  const response: ResponseArray<Author> = await axios.get(
+    `${ENDPOINT}${filters}`
+  )
+  return response.data.data
 }
